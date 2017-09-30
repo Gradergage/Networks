138d2c3a3602da1259e0736efa32bcc9481d23db
Pokatilo Pavel <gradergage@gmail.com>
2017-09-30 16:53:10 +0300

    Working server to gen individual task

--- Server_TCP/server.cpp
+++ Server_TCP/server.cpp
@@ -5,149 +5,160 @@
 #include <thread>
 #include <vector>
 #include <conio.h>
+#include <pthread.h>
 #include <mutex>
 #define DEFAULT_BUFLEN 512
 #define MAX_THREADS 10
-#define _WIN32_WINNT 0x0501
 
-std::mutex hMutex;
 
-bool serverTerminated=false;
-bool acceptTerminated =false;
-int currentClientCount=0;
+std::mutex vMutex; //Client's vector mutex
+std::mutex cMutex; //Console mutex
 
+
+//Main structure which defines client parameters
 struct CLIENT
 {
     int id;
-    SOCKET * sock;
+    SOCKET sock;
     sockaddr_in addr;
+    bool connected;
+    std::thread * cTh;
 };
-
-std::vector <CLIENT *> clients;
-
+std::vector <CLIENT *> clients; //Vector of clients structures
 int readn(SOCKET & sock, char *recvbuf, int recvbuflen)
 {
         int iResult=0;
-        memset(recvbuf,NULL,recvbuflen);
+        memset(recvbuf,0,recvbuflen);
         int recievedBytes=0;
 
         while(recievedBytes<recvbuflen)
         {
 
             iResult = recv( sock, recvbuf+recievedBytes, recvbuflen-recievedBytes, 0 );
+            if (iResult == INVALID_SOCKET) {
+                return INVALID_SOCKET;
+            }
             if (iResult == SOCKET_ERROR) {
-                return 1;
+                return SOCKET_ERROR;
             }
             recievedBytes+=iResult;
         }
         return 0;
 }
 
+// IP and Port of socket
 void socketInfo(sockaddr_in & client_info)
 {
-    int iResult;
     char *connected_ip = inet_ntoa(client_info.sin_addr);
     int port = ntohs(client_info.sin_port);
     printf("%s:%d\n", connected_ip,port);
 }
+
+// Disconnecting and clearing memory of client
 bool disconnectClientById(int id)
 {
-    hMutex.lock();
-    for(int i=0; i<clients.size() ; i++)
+    vMutex.lock();
+    for(unsigned int i=0; i<clients.size() ; i++)
     {
         if(clients[i]->id==id)
         {
-            shutdown(*clients[i]->sock, 2);
-            closesocket(*clients[i]->sock);
+            clients[i]->connected=false;
+            shutdown(clients[i]->sock, 2);
+            closesocket(clients[i]->sock);
             delete(clients[i]);
             clients.erase(clients.begin()+i);
+            cMutex.lock();
             printf("Client %d has been disconnected\n",id);
-            currentClientCount--;
-            hMutex.unlock();
+            cMutex.unlock();
+            vMutex.unlock();
             return true;
         }
     }
-    hMutex.unlock();
+    vMutex.unlock();
     return false;
 }
+
+// Checking of existing object
 bool findById(int id)
 {
-    hMutex.lock();
-    for(int i=0; i<clients.size() ; i++)
+    vMutex.lock();
+    for(unsigned int i=0; i<clients.size() ; i++)
     {
         if(clients[i]->id==id)
         {
-            hMutex.unlock();
+            vMutex.unlock();
             return true;
         }
     }
-    hMutex.unlock();
+    vMutex.unlock();
     return false;
 }
 
+// Client thread which exchanges messages and queries
 int handleClient(CLIENT *client)
 {
-    sockaddr_in addr = client->addr;
-    SOCKET cSocket = *client->sock;
-    int iResult;
+    SOCKET cSocket = client->sock;
+    unsigned int iResult;
     int cId=client->id;
-    char *connected_ip = inet_ntoa(addr.sin_addr);
-    int port = ntohs(addr.sin_port);
-    bool terminated=false;
-
     char recvbuf[DEFAULT_BUFLEN];
     char sendbuf[DEFAULT_BUFLEN];
 
-    while(!terminated)
+    while(true)
     {
         iResult = readn(cSocket, recvbuf, sizeof(recvbuf));
-        if(strcmp(recvbuf,"-close")==0)
+        if(iResult==SOCKET_ERROR||iResult==INVALID_SOCKET)
         {
-            disconnectClientById(cId);
-            terminated=true;
             return 0;
         }
-        if (iResult == SOCKET_ERROR||cSocket==INVALID_SOCKET) {
-            wprintf(L"send failed with error: %d\n", WSAGetLastError());
+        if(strcmp(recvbuf,"-close")==0)
+        {
+
             disconnectClientById(cId);
-            terminated=true;
-            return 1;
+            return 0;
         }
         else
         {
-            hMutex.lock();
-            printf("Message [ %s ] from %s:%d\n",recvbuf,connected_ip,port);
-            hMutex.unlock();
+                cMutex.lock();
+                if(client->connected)
+                // printf("Message \"%s\" from %s:%d\n",recvbuf,connected_ip,port);
+                printf("- Message \"%s\" from %d\n",recvbuf,cId);
+                memset(recvbuf,0,DEFAULT_BUFLEN);
+                cMutex.unlock();
         }
-        if(!terminated)
+        if(client->connected)
         {
             char responcebuf[DEFAULT_BUFLEN]="Response";
-            if(!findById(cId))
-            {
-                return 0;
+
+            vMutex.lock();
+            iResult = send( cSocket, responcebuf, DEFAULT_BUFLEN, 0 );
+            if (iResult == SOCKET_ERROR|| iResult==INVALID_SOCKET) {
+                printf("send failed: %d\n", WSAGetLastError());
+                disconnectClientById(cId);
+                return 1;
             }
-            else
-            {
-                iResult = send( cSocket, responcebuf, DEFAULT_BUFLEN, 0 );
-                if (iResult == SOCKET_ERROR) {
-                    printf("-send failed: %d\n", WSAGetLastError());
-                    disconnectClientById(cId);
-                    return 1;
-                }
-            }
+            vMutex.unlock();
+        }
+        else
+        {
+            disconnectClientById(cId);
+            return 0;
         }
     }
     return 0;
 }
+
+// List of all clients with id
 void showAllClients()
 {
-    hMutex.try_lock();
+    cMutex.lock();
     printf("\nClients connected:\n\n");
     int i=0;
+    vMutex.lock();
     if (clients.empty())
     {
         printf("EMPTY\n");
-        hMutex.unlock();
+        vMutex.unlock();
+        cMutex.unlock();
         return;
     }
     for ( CLIENT * n : clients ) {
@@ -155,23 +166,32 @@ void showAllClients()
         printf("%d) id:%d ",i,n->id);
         socketInfo((sockaddr_in &)n->addr);
     }
+    vMutex.unlock();
     printf("\n");
-    hMutex.unlock();
+    cMutex.unlock();
 }
-
-int serverThread()
+int disconnectAll()
 {
-    char* command="";
-    while(!::serverTerminated)
+    for(CLIENT *n : clients)
     {
-        printf("MENU \n1 - show all clients menu\n2 - close connection with some client\n3 - close all connections\n4 - shutdown server\n");
-        std::cin>>command;
-        if(strcmp(command,"-close")==0)
-        {
-            ::serverTerminated=true;
-        }
+
+            int cId=n->id;
+            shutdown(n->sock, 2);
+            closesocket(n->sock);
+            n->connected=false;
+            n->cTh->join();
+            delete(n->cTh);
+            delete(n);
+            vMutex.lock();
+            vMutex.unlock();
+            cMutex.lock();
+            printf("Client %d has been disconnected\n",cId);
+            cMutex.unlock();
     }
+    clients.erase(clients.begin(),clients.end());
+    return 0;
 }
+// Unique id from ip and port
 int hashByIp(char* ip, int len, int port)
 {
     int sum=1;
@@ -182,37 +202,55 @@ int hashByIp(char* ip, int len, int port)
     }
     return sum+port;
 }
+
+// Function accepting new clients and providing new threads to them
 int acceptThread(SOCKET & sock)
 {
-    while(!acceptTerminated)
-   {
-        hMutex.lock();
-        printf("-Accepting \n");
-        hMutex.unlock();
 
+    while(true)
+   {
         SOCKET acceptSocket;
         CLIENT * entrySocket=new CLIENT();
         sockaddr_in client_info={0};
-
         int len = sizeof(client_info);
+        if(sock==INVALID_SOCKET || sock==SOCKET_ERROR)
+        {
+            break;
+        }
+        else
+        {
+            acceptSocket = accept(sock, (SOCKADDR *) & client_info, & len);
+            if(acceptSocket==INVALID_SOCKET)
+            {
+                break;
+            }
+        }
 
-        acceptSocket = accept(sock, (SOCKADDR *) & client_info, & len);
-
-        hMutex.lock();
-
-        //attaching socket's info to socket itself
+        // Attaching socket's info to socket itself
         char* innerIp=inet_ntoa(client_info.sin_addr);
         entrySocket->addr = client_info;
-        entrySocket->sock = &acceptSocket;
-        entrySocket->id=hashByIp(innerIp,sizeof(innerIp),client_info.sin_port);
+        entrySocket->sock = acceptSocket;
+        entrySocket->connected=true;
+        int id=hashByIp(innerIp,sizeof(innerIp),client_info.sin_port);
+        entrySocket->id=id;
+        int port=ntohs(entrySocket->addr.sin_port);
 
+        std::thread *sockTh= new std::thread(handleClient, std::ref(entrySocket));
+        entrySocket->cTh=sockTh;
+
+        vMutex.lock();
         clients.push_back(entrySocket);
-        currentClientCount++;
+        vMutex.unlock();
 
-        std::thread sockTh(handleClient, std::ref(entrySocket));
-        sockTh.detach();
-        hMutex.unlock();
+        cMutex.lock();
+        printf("New client %s:%d connected using id: %d\n", innerIp,port,id);
+        cMutex.unlock();
    }
+
+   disconnectAll();
+   cMutex.lock();
+   printf("Accepting closed\n");
+   cMutex.unlock();
    return 0;
 }
 
@@ -221,14 +259,12 @@ int main()
     WSADATA wsaData = {0};
 
     int iResult=0;
-    int threadId[MAX_THREADS];
     int iFamily = AF_INET;
     int iType = SOCK_STREAM;
     int iProtocol = IPPROTO_TCP;
 
-    char *IP_ADDRESS = "127.0.0.1";
+    char IP_ADDRESS[] = "127.0.0.1";
     int PORT=27015;
-    int recvbuflen = 512;
 
     SOCKET sock = INVALID_SOCKET;
 
@@ -254,10 +290,10 @@ int main()
 
     iResult = bind(sock, (SOCKADDR *) & service, sizeof (service));
     if (iResult == SOCKET_ERROR) {
-        printf("-bind function failed with error %d\n", WSAGetLastError());
+        printf("bind function failed with error %d\n", WSAGetLastError());
         iResult = closesocket(sock);
         if (iResult == SOCKET_ERROR)
-            printf("-closesocket function failed with error %d\n", WSAGetLastError());
+            printf("closesocket function failed with error %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
@@ -268,14 +304,17 @@ int main()
     if (listen(sock, SOMAXCONN) == SOCKET_ERROR)
         wprintf(L"listen function failed with error: %d\n", WSAGetLastError());
 
-    std::thread acceptTh(acceptThread, std::ref(sock));
-    acceptTh.detach();
 
-    while(!::serverTerminated)
+
+    // Starting accept thread
+    std::thread acceptTh(acceptThread, std::ref(sock));
+
+    // Server menu working in main thread
+    while(true)
     {
-        hMutex.lock();
-        printf("\nMENU \n1 - show all clients menu\n2 - close connection with some client\n3 - close all connections\n4 - shutdown server\n");
-        hMutex.unlock();
+        cMutex.lock();
+        printf("\nMENU \n1 - show all clients menu\n2 - close connection with some client\n3 - shutdown server\n\n");
+        cMutex.unlock();
 
         char choise = getch();
 
@@ -301,11 +340,26 @@ int main()
                 }
                 break;
                 }
-            case '3':{
+            case '4':{
 
                 break;}
-            case '4':{
-                ::serverTerminated=true;
+            case '3':{
+                cMutex.lock();
+                printf("\nServer is closing...\n");
+                cMutex.unlock();
+
+                shutdown(sock,2);
+                closesocket(sock);
+
+                cMutex.lock();
+                printf("Accepting is closing...\n");
+                cMutex.unlock();
+
+                acceptTh.join();
+
+                cMutex.lock();
+                printf("Server closed\n");
+                cMutex.unlock();
                 return 0;
                 break;}
         }
